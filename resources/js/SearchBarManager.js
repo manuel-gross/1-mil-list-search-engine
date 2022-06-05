12ecfae5a4f9b6a4be1ecfa6a5e87674193975d7
Manuel <Manuel.Gross@stud.uni-regensburg.de>
2022-06-05 15:05:22 +0200

    adding search conditions works now

--- resources/js/SearchBarManager.js
+++ resources/js/SearchBarManager.js
@@ -1,16 +1,15 @@
 function addSearchInput(){
     let el = document.createElement("div");
     el.innerHTML = `<li>
-    <input type="text" class="searchControls" placeholder="Search ...">
-    <select name="searchFor">
+    <input type="text" id="searchInput" class="searchControls" placeholder="Search ...">
+    <select name="searchFor" id="searchFor">
         <option value="id">ID</option>
         <option value="content">Content</option>
         <option value="title">Title</option>
         <option value="media-type">Media Type</option>
         <option value="source">Source</option>
         <option value="published">Published</option>
-    </select>
-    </li>`
+    </select>`;
     $("#searchCondition").prepend(el);
 }
 
